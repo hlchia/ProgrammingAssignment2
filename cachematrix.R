72cf7eebd946f4d6b9f54abf9901827f2cc48d15
Chia Hock Lai <chiahl2@gmail.com>
2014-08-24 10:10:24 +0800

    Submit the solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## object that returns a list containing functions to set and get a matrix, 
+## get and set inverse of a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
+	    ## global variable to store inversed matrix
 	    i <- NULL
+	    ## set the 
         set <- function(y) {
                 x <<- y
                 i <<- NULL
@@ -20,18 +23,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## object that stores a matrix and and caches the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## get the global variable that is used to store a cahce of the matrix inverse
          i <- x$getinverse()
+        ## check if the global vairable has a value, if yes then return the variable
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+        ## if the global vairbale is empty, then get the matrix
         data <- x$get()
+        ## inverses the matrix and store it in a variable
         i <- solve(data, ...)
+
         x$setinverse(i)
+        ## Return a matrix that is the inverse of 'x'
         i
 
 }
